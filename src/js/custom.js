73cdcf4748f46eb434934f8bb0c643f534154aa1
Chandan Singh <avichandan.singh@gmail.com>
2023-01-31 20:35:49 +0530

    entities details

--- src/js/custom.js
+++ src/js/custom.js
@@ -10,6 +10,36 @@ $(function () {
     "pagingType": "simple"
   });
 
+  $('#overdue-table').DataTable({
+    "searching": false,
+    "lengthChange": false,
+    "pagingType": "simple"
+  });
+
+  $('#upcoming-table').DataTable({
+    "searching": false,
+    "lengthChange": false,
+    "pagingType": "simple"
+  });
+
+  $('#sumbitted-table').DataTable({
+    "searching": false,
+    "lengthChange": false,
+    "pagingType": "simple"
+  });
+
+  $('#setToState-table').DataTable({
+    "searching": false,
+    "lengthChange": false,
+    "pagingType": "simple"
+  });
+
+  $('#upToDate-table').DataTable({
+    "searching": false,
+    "lengthChange": false,
+    "pagingType": "simple"
+  });
+
   $('#jurisdictions-listing').DataTable({
     "searching": false,
     "lengthChange": false,
@@ -24,18 +54,20 @@ $(function () {
 
   $('.nav-item a').on('click', function () {
     let dataId = $(this).attr('data-id');
-    $('.tab-content-wrapper').css('display', 'none');
-    $('#' + dataId).css('display', 'block');
-    if (dataId === 'grid-view') {
-      $('.nav-items').css({
-        'opacity': '0',
-        'visibility': 'hidden'
-      });
-    } else {
-      $('.nav-items').css({
-        'opacity': '1',
-        'visibility': 'visible'
-      });
+    if (dataId === 'grid-view' || dataId === 'list-view') {
+      $('.tab-content-wrapper').css('display', 'none');
+      $('#' + dataId).css('display', 'block');
+      if (dataId === 'grid-view') {
+        $('.nav-items').css({
+          'opacity': '0',
+          'visibility': 'hidden'
+        });
+      } else {
+        $('.nav-items').css({
+          'opacity': '1',
+          'visibility': 'visible'
+        });
+      }
     }
   });
 });
