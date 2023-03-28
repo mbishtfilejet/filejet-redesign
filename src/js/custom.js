927ba77cb19523c58b53a807d92dd7509a6e2f03
Tarun Jain <tarunjain3820@gmail.com>
2023-03-28 16:41:52 +0530

    fix: updated hover state for state field in all filter slideouts

--- src/js/custom.js
+++ src/js/custom.js
@@ -305,4 +305,17 @@ function closeAllSelect(elmnt) {
 /*if the user clicks anywhere outside the select box,
 then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
-/*Custom Select End*/
+
+//hover state for state field in filter slideout
+$('.filter-state-label').hover(
+  function () {
+    var $this = $(this); // caching $(this)
+    title = $(this).attr('title');
+    $this.data('defaultText', $this.text());
+    $this.text(title);
+  },
+  function () {
+    var $this = $(this); // caching $(this)
+    $this.text($this.data('defaultText'));
+  }
+);
