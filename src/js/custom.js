65724079d02f1594a01ae88d8ce0f81b21ac2d63
Tarun Jain <tarunjain3820@gmail.com>
2023-04-01 17:28:13 +0530

    feat: added file upload in groups and bl nested table

--- src/js/custom.js
+++ src/js/custom.js
@@ -144,51 +144,54 @@ $(function () {
   });
 });
 
-document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
-  const dropZoneElement = inputElement.closest(".drop-zone");
-
-  dropZoneElement.addEventListener("click", (e) => {
-    inputElement.click();
-  });
-
-  inputElement.addEventListener("change", (e) => {
-    if (inputElement.files.length) {
-      updateThumbnail(dropZoneElement, inputElement.files[0]);
-    }
-  });
-
-  dropZoneElement.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    dropZoneElement.classList.add("drop-zone--over");
-  });
-
-  ["dragleave", "dragend"].forEach((type) => {
-    dropZoneElement.addEventListener(type, (e) => {
+function fileUploadInput(){
+  document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+  
+    dropZoneElement.addEventListener("click", (e) => {
+      inputElement.click();
+    });
+  
+    inputElement.addEventListener("change", (e) => {
+      if (inputElement.files.length) {
+        updateThumbnail(dropZoneElement, inputElement.files[0]);
+      }
+    });
+  
+    dropZoneElement.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      dropZoneElement.classList.add("drop-zone--over");
+    });
+  
+    ["dragleave", "dragend"].forEach((type) => {
+      dropZoneElement.addEventListener(type, (e) => {
+        dropZoneElement.classList.remove("drop-zone--over");
+      });
+    });
+  
+    dropZoneElement.addEventListener("drop", (e) => {
+      e.preventDefault();
+  
+      if (e.dataTransfer.files.length) {
+        inputElement.files = e.dataTransfer.files;
+        updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+      }
+  
       dropZoneElement.classList.remove("drop-zone--over");
     });
+  
+    $(document).on('click', '.go-back-parent', function () {
+      $('.folder-level').css('display', 'none');
+      $('#parent-folder').css('display', 'block');
+    });
+  
+    $(document).on('click', '.go-back-child', function () {
+      $('.folder-level').css('display', 'none');
+      $('#child-folder').css('display', 'block');
+    });
   });
-
-  dropZoneElement.addEventListener("drop", (e) => {
-    e.preventDefault();
-
-    if (e.dataTransfer.files.length) {
-      inputElement.files = e.dataTransfer.files;
-      updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-    }
-
-    dropZoneElement.classList.remove("drop-zone--over");
-  });
-
-  $(document).on('click', '.go-back-parent', function () {
-    $('.folder-level').css('display', 'none');
-    $('#parent-folder').css('display', 'block');
-  });
-
-  $(document).on('click', '.go-back-child', function () {
-    $('.folder-level').css('display', 'none');
-    $('#child-folder').css('display', 'block');
-  });
-});
+}
+fileUploadInput();
 
 /**
  * Updates the thumbnail on a drop zone element.
