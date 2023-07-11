e9779580542988ea66d0f9b37ca16c12bd986c19
Vivek Pathak <44230234+developervivek1@users.noreply.github.com>
2023-07-11 17:45:00 +0530

    custom toolbar created in bottom for org-chart.js (#105)
    custom toolbar created in bottom for org-chart.js

--- src/js/custom-org-chart.js
+++ src/js/custom-org-chart.js
@@ -1,4 +1,22 @@
 //JavaScript
+// var layoutIcon ='./dist/images/icons/hunt-groups.svg';
+var expandAllIcon = '<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+    '<path d="M5.11249 9.9486C5.37284 9.68825 5.79495 9.68825 6.05529 9.9486C6.31564 10.2089 6.31564 10.6311 6.05529 10.8914L4.27667 12.6695H5.33333C5.67522 12.6695 5.957 12.9268 5.99551 13.2584L6 13.3361C6 13.7043 5.70152 14.0028 5.33333 14.0028H2.66667C2.29848 14.0028 2 13.7043 2 13.3361V10.6695C2 10.3013 2.29848 10.0028 2.66667 10.0028C3.03486 10.0028 3.33333 10.3013 3.33333 10.6695V11.7275L5.11249 9.9486ZM10.67 14.0028C10.3018 14.0028 10.0033 13.7043 10.0033 13.3361C10.0033 12.968 10.3018 12.6695 10.67 12.6695H11.7253L9.94841 10.8912C9.70818 10.6508 9.68984 10.2726 9.8933 10.0112L9.94878 9.94841C10.2092 9.68816 10.6313 9.68833 10.8916 9.94878L12.67 11.7288V10.6695C12.67 10.3276 12.9274 10.0458 13.2589 10.0073L13.3367 10.0028C13.7049 10.0028 14.0033 10.3013 14.0033 10.6695V13.3361C14.0033 13.7043 13.7049 14.0028 13.3367 14.0028H10.67ZM5.33333 2C5.70152 2 6 2.29848 6 2.66667C6 3.03486 5.70152 3.33333 5.33333 3.33333H4.278L6.05492 5.11211C6.29515 5.35253 6.31349 5.73069 6.11004 5.99214L6.05455 6.05492C5.7941 6.31517 5.37199 6.315 5.11174 6.05455L3.33333 4.27467V5.33333C3.33333 5.67522 3.07597 5.957 2.74441 5.99551L2.66667 6C2.29848 6 2 5.70152 2 5.33333V2.66667C2 2.29848 2.29848 2 2.66667 2H5.33333ZM13.3367 2C13.7049 2 14.0033 2.29848 14.0033 2.66667V5.33333C14.0033 5.70152 13.7049 6 13.3367 6C12.9685 6 12.67 5.70152 12.67 5.33333V4.27467L10.8915 6.05464C10.6512 6.29501 10.2731 6.31358 10.0115 6.11028L9.94869 6.05483C9.68829 5.79454 9.6882 5.37243 9.9485 5.11202L11.726 3.33333H10.67C10.3281 3.33333 10.0463 3.07597 10.0078 2.74441L10.0033 2.66667C10.0033 2.29848 10.3018 2 10.67 2H13.3367Z" fill="#212121"/>' +
+    '</svg>';
+
+var fitIcon = '<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+    '<path d="M4.5 3C3.67157 3 3 3.67157 3 4.5V5.5C3 5.77614 2.77614 6 2.5 6C2.22386 6 2 5.77614 2 5.5V4.5C2 3.11929 3.11929 2 4.5 2H5.5C5.77614 2 6 2.22386 6 2.5C6 2.77614 5.77614 3 5.5 3H4.5ZM5.5 4C4.67157 4 4 4.67157 4 5.5V10.5C4 11.3284 4.67157 12 5.5 12H10.5C11.3284 12 12 11.3284 12 10.5V5.5C12 4.67157 11.3284 4 10.5 4H5.5ZM11.5 3C12.3284 3 13 3.67157 13 4.5V5.5C13 5.77614 13.2239 6 13.5 6C13.7761 6 14 5.77614 14 5.5V4.5C14 3.11929 12.8807 2 11.5 2H10.5C10.2239 2 10 2.22386 10 2.5C10 2.77614 10.2239 3 10.5 3L11.5 3ZM11.5 13C12.3284 13 13 12.3284 13 11.5V10.5C13 10.2239 13.2239 10 13.5 10C13.7761 10 14 10.2239 14 10.5V11.5C14 12.8807 12.8807 14 11.5 14H10.5C10.2239 14 10 13.7761 10 13.5C10 13.2239 10.2239 13 10.5 13H11.5ZM4.5 13C3.67157 13 3 12.3284 3 11.5L3 10.25C3 9.97386 2.77614 9.75 2.5 9.75C2.22386 9.75 2 9.97386 2 10.25V11.5C2 12.8807 3.11929 14 4.5 14H5.75C6.02614 14 6.25 13.7761 6.25 13.5C6.25 13.2239 6.02614 13 5.75 13H4.5Z" fill="#212121"/>' +
+    '</svg>';
+
+var zoomInIcon = '<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+    '<path d="M7.25 14.25C7.25 14.6642 7.58579 15 8 15C8.41421 15 8.75 14.6642 8.75 14.25V8.75H14.25C14.6642 8.75 15 8.41421 15 8C15 7.58579 14.6642 7.25 14.25 7.25H8.75V1.75C8.75 1.33579 8.41421 1 8 1C7.58579 1 7.25 1.33579 7.25 1.75V7.25H1.75C1.33579 7.25 1 7.58579 1 8C1 8.41421 1.33579 8.75 1.75 8.75H7.25V14.25Z" fill="#212121"/>' +
+    '</svg>';
+
+
+var zoomOutIcon = '<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+    '<rect x="1" y="7.25" width="14" height="1.5" rx="0.75" fill="#212121"/>' +
+    '</svg>';
+
 var actionIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">' +
     '<path d="M2.56367 12.2571L7.56524 3.25712C7.73988 2.94286 8.16874 2.91667 8.38575 3.17856L8.43933 3.25712L13.4409 12.2571C13.6119 12.5648 13.4197 12.9374 13.0892 12.993L13.0039 13H3.00072C2.64878 13 2.41637 12.651 2.52837 12.3352L2.56367 12.2571L7.56524 3.25712L2.56367 12.2571ZM8.00229 9.87694C7.58783 9.87694 7.25185 10.2129 7.25185 10.6274C7.25185 11.0418 7.58783 11.3778 8.00229 11.3778C8.41674 11.3778 8.75273 11.0418 8.75273 10.6274C8.75273 10.2129 8.41674 9.87694 8.00229 9.87694ZM8.002 6.87757C7.6882 6.87733 7.42824 7.10838 7.38337 7.40973L7.37652 7.50209L7.37576 8.49952L7.38247 8.59188C7.42688 8.8933 7.68648 9.12476 8.00028 9.125C8.31408 9.12524 8.57404 8.89418 8.61891 8.59283L8.62576 8.50048L8.62652 7.50304L8.61982 7.41068C8.57541 7.10926 8.3158 6.87781 8.002 6.87757Z"/>' +
     '<defs>' +
@@ -37,6 +55,12 @@ OrgChart.templates.olivia.img_0 =
     + '</clipPath>'
     + '<image preserveAspectRatio="xMidYMid slice" clip-path="url(#ulaImg)" xlink:href="{val}" x="14" y="15" width="65" height="65">'
     + '</image>';
+OrgChart.toolbarUI.expandAllIcon = expandAllIcon;
+OrgChart.toolbarUI.fitIcon = fitIcon;
+OrgChart.toolbarUI.zoomInIcon = zoomInIcon;
+OrgChart.toolbarUI.zoomOutIcon = zoomOutIcon;
+// OrgChart.toolbarUI.layoutIcon = layoutIcon;
+
 OrgChart.templates.olivia.size = [320, 92];
 OrgChart.templates.olivia.nodeMenuButton = '<g style="cursor:pointer;background-color:blue;" transform="matrix(1,0,0,1,285,47)" data-ctrl-n-menu-id="{id}"><rect x="-4" y="-10" fill="#000000" fill-opacity="0" width="22" height="22"></rect><circle cx="0" cy="0" r="2" fill="#ffffff"></circle><circle cx="7" cy="0" r="2" fill="#ffffff"></circle><circle cx="14" cy="0" r="2" fill="#ffffff"></circle></g>';
 OrgChart.templates.olivia.link = '<path stroke-linejoin="round" stroke="#aeaeae" stroke-width="1px" fill="none" d="{rounded}" />';
@@ -45,8 +69,16 @@ var chart = new OrgChart(document.getElementById("tree"), {
     showYScroll: OrgChart.scroll.visible,
     showXScroll: OrgChart.scroll.visible,
     mouseScrool: OrgChart.action.ctrlZoom,
-    layout: OrgChart.mixed,
+    scaleInitial: OrgChart.match.boundary,
     enableSearch: false,
+    // scaleInitial: OrgChart.match.height,
+    layout: OrgChart.mixed,
+    toolbar: {
+        // layout: true,
+        zoom: true,
+        fit: true,
+        expandAll: true
+    },
     nodeMenu: {
         action: {
             text: "Take Action",
@@ -78,7 +110,9 @@ var chart = new OrgChart(document.getElementById("tree"), {
         warning: "warning"
     }
 });
-
+// chart.on('init', function (sender) {
+//     sender.toolbarUI.showLayout();
+// });
 chart.load([
     { id: "1", pid: "0", company: "Hunt Groups", name: "Jack Hill", title: "Chairman and CEO", title2: "Google", email: "amber@domain.com", img: "dist/images/icons/hunt-groups.svg", warning: "#", },
     { id: "2", pid: "1", company: "Hunt LLC", name: "Lexie Cole", title: "QA Lead", email: "ava@domain.com", img: "dist/images/icons/hunt-groups.svg", warning: "#", },
