0c9e5af6ddd45e994db91c29b08301a2e792c257
Vivek Pathak <44230234+developervivek1@users.noreply.github.com>
2024-06-08 17:45:24 +0530

    fix clink feature disabled for subsidiary chart (#402)
    fix clink feature disabled for subsidiary chart

--- src/js/custom-org-chart-parent-child.js
+++ src/js/custom-org-chart-parent-child.js
@@ -237,10 +237,10 @@ var chart = new OrgChart(document.getElementById("tree"), {
         // Statu:"Status",
     },
     
-    clinks: [
-        { from: 2, to: 4, template: 'blue', label: 'Sub Parent to Unmanaged' },
-        { from: 10, to: 7, template: 'yellow', label: 'Sub-children2 to child-2 22222' },
-    ],
+    // clinks: [
+    //     { from: 2, to: 4, template: 'blue', label: 'Sub Parent to Unmanaged' },
+    //     { from: 10, to: 7, template: 'yellow', label: 'Sub-children2 to child-2 22222' },
+    // ],
     
     editUI: new editForm(),
     // edit form
