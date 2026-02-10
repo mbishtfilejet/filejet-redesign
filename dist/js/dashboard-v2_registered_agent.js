7cfd71ec1deb2013e3ef6b2e27ec5dd204062713
Tushar Kaundal <tkaunda@filejet.com>
2026-02-10 21:04:36 +0530

    updated Registered Agent table as per feedbacks

--- dist/js/dashboard-v2_registered_agent.js
+++ dist/js/dashboard-v2_registered_agent.js
@@ -1279,7 +1279,7 @@ $(document).ready(function () {
       }
     ],
     scrollX: true,
-    order: [[0, "asc"]],
+    order: [[3, "desc"]],
     lengthChange: false,  // Removed pagination
     paging: false,  // Disable pagination
     info: false,    // Hide table info (e.g., "Showing 1 to 10 of 50 entries"
