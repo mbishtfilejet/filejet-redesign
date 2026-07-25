98b04f5c1e04e2d4f78098f35f4e5eb42a403e39
Tushar Kaundal <tkaunda@filejet.com>
2026-07-25 16:43:36 +0530

    update: changed filter for subscription table as per feedback

--- dist/js/dashboard-v2_document.js
+++ dist/js/dashboard-v2_document.js
@@ -317,8 +317,8 @@ document.addEventListener("DOMContentLoaded", function () {
         case "groupUserRoleContainer": return "Filter by Role";
         case "usersAccessContainer":
         case "externalUserContainer": return "Filter by Access"
-        case "groupPaymentContainer":
-        case "groupContainer": return "Available To"
+        case "groupPaymentContainer": return "Available To";
+        case "groupContainer": return "Group";
         default: return "Status";
       }
     }
